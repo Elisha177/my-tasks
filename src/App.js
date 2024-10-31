3a7b21ed2fffedcc1f3e8e603ca05abd22c37026
Elisha <Elisha177>
2024-10-31 11:55:46 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-named-as-default-member */
+import './App.css'
+import Home from './components/Home'
+
+// These are the lists used in the application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => <Home />
+
+export default App
